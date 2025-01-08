ee491b8e33375231b2e165d7abab70a476e0e115
Denis Mamaev <md222@yandex.ru>
2025-01-08 18:37:22 +0400

    3.1.4

--- src/main/java/ru/kata/spring/boot_security/demo/configs/WebSecurityConfig.java
+++ src/main/java/ru/kata/spring/boot_security/demo/configs/WebSecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -15,6 +16,7 @@ import ru.kata.spring.boot_security.demo.services.UserService;
 
     @Configuration
     @EnableWebSecurity
+    @EnableMethodSecurity
     public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         private final SuccessUserHandler successUserHandler;
         private final UserService userService;
